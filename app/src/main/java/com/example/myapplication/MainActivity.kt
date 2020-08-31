095b8e50591929ffe2d1c16ffee9c72a76f94f19
Simon <coopersimon@gmail.com>
2020-08-31 03:46:26 +0100

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myShapeAnimation: AnimatedVectorDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myAnim = findViewById<ImageView>(R.id.imageView2).apply {
+            setImageDrawable(getDrawable(R.drawable.shape_vector_animator))
+            myShapeAnimation = getDrawable() as AnimatedVectorDrawable
+        }
+        myAnim.setOnClickListener({ myShapeAnimation.start() })
     }
+
+
+
 }
+
